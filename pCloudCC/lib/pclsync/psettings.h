ad8427118c9ce1f14465700a28c248fc8ebe2442
alworx <alworx@gmx.net>
2020-12-09 00:37:56 +0100

    changed api host to eu servers

--- pCloudCC/lib/pclsync/psettings.h
+++ pCloudCC/lib/pclsync/psettings.h
@@ -40,11 +40,11 @@
 #define PSYNC_API_PORT_SSL 8399
 */
 
-#define PSYNC_API_HOST     "binapi.pcloud.com"
+#define PSYNC_API_HOST     "bineapi.pcloud.com"
 #define PSYNC_API_PORT     80
 #define PSYNC_API_PORT_SSL 443
 
-#define PSYNC_API_AHOST     "api.pcloud.com"
+#define PSYNC_API_AHOST     "eapi.pcloud.com"
 #define PSYNC_API_APORT     8398
 #define PSYNC_API_APORT_SSL 8399
 
